0a2e5edcf9c86f0832143f0827c27e409d023cf2
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-12-23 20:59:31 +0100

    wip barcode markers generator, added utils

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export const toBase64 = (file) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
