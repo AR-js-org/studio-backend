4872fec8e7184ecf0a51011e56aeb88ce2876b0d
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-12-23 18:36:27 +0100

    first commit 🤩

---
+++ index.js
@@ -0,0 +1,10 @@
+import { BarcodeMarkersGenerator } from './src/tools/markers/barcode-generator.js';
+import { PatternMarkersGenerator} from './src/tools/markers/pattern-generator.js';
+
+export class StudioBackend {
+    static getBarcodeMarker(matrixName, value) {
+        // return BarcodeMarkersGenerator.getBarcodeMarker(matrixName, value);
+    }
+
+    // ....
+}
