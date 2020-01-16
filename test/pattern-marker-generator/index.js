ec0ac222f86ce27baeee6ed1bd45b480bc8ed8db
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-01-16 09:28:34 +0100

    wip tests, missing a part of the generator

---
+++ test/pattern-marker-generator/index.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import * as generator from '../../src/tools/markers/pattern-marker-generator.js';
+
+describe('pattern-marker-generator', function () {
+    it('correctly generates pattern marker', function () {
+       // TODO
+    });
+
+    it('correctly generates pattern image', function () {
+        // TODO
+     });
+});
