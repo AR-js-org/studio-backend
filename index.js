4c8092db4fb2d2b70e4c01fd326baad4f57b4df7
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-01-16 09:14:23 +0100

    correct code indentation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import {BarcodeMarkerGenerator} from './src/tools/markers/barcode-marker-generator';
-import {PatternMarkerGenerator} from './src/tools/markers/pattern-marker-generator';
+import { BarcodeMarkerGenerator } from './src/tools/markers/barcode-marker-generator';
+import { PatternMarkerGenerator } from './src/tools/markers/pattern-marker-generator';
 
 export class StudioBackend {
-  static getBarcodeMarkerSVGDataURI(matrixTypeId, value) {
-    return new BarcodeMarkerGenerator(matrixTypeId, value).asSVGDataURI();
-  }
+    static getBarcodeMarkerSVGDataURI(matrixTypeId, value) {
+        return new BarcodeMarkerGenerator(matrixTypeId, value).asSVGDataURI();
+    }
 
-  static async getMarkerPattern(dataURI) {
-    return await new PatternMarkerGenerator(dataURI).toPattern();
-  }
+    static async getMarkerPattern(dataURI) {
+        return await new PatternMarkerGenerator(dataURI).toPattern();
+    }
 }
--- src/tools/markers/pattern-marker-generator.js
+++ src/tools/markers/pattern-marker-generator.js
@@ -1,51 +1,51 @@
 import { loadImage } from './helpers.js';
 
 export class PatternMarkerGenerator {
-  constructor (dataURI) {
-    if (!dataURI.startsWith('data:image'))
-      throw new Error('Invalid data URI');
+    constructor(dataURI) {
+        if (!dataURI.startsWith('data:image'))
+            throw new Error('Invalid data URI');
 
-    this.dataURI = dataURI;
-  }
-
-  async toPattern() {
-    const image = await loadImage(this.dataURI);
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = 16;
-    canvas.height = 16;
-
-    let patternString = '';
-
-    for (let orientation = 0; orientation > -2 * Math.PI; orientation -= Math.PI / 2) {
-      context.save();
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.translate(canvas.width / 2, canvas.height / 2);
-      context.rotate(orientation);
-      context.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-      context.restore();
-
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-      if (orientation !== 0)
-        patternString += '\n';
-
-      for (let channelOffset = 2; channelOffset >= 0; channelOffset--) {
-        for (let y = 0; y < imageData.height; y++) {
-          for (let x = 0; x < imageData.width; x++) {
-            if (x !== 0)
-              patternString += ' '
-
-            const offset = (y * imageData.width * 4) + (x * 4) + channelOffset;
-            const value = imageData.data[offset];
-            patternString += String(value).padStart(3);
-          }
-
-          patternString += '\n';
-        }
-      }
+        this.dataURI = dataURI;
     }
 
-    return patternString;
-  }
+    async toPattern() {
+        const image = await loadImage(this.dataURI);
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = 16;
+        canvas.height = 16;
+
+        let patternString = '';
+
+        for (let orientation = 0; orientation > -2 * Math.PI; orientation -= Math.PI / 2) {
+            context.save();
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.translate(canvas.width / 2, canvas.height / 2);
+            context.rotate(orientation);
+            context.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+            context.restore();
+
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+            if (orientation !== 0)
+                patternString += '\n';
+
+            for (let channelOffset = 2; channelOffset >= 0; channelOffset--) {
+                for (let y = 0; y < imageData.height; y++) {
+                    for (let x = 0; x < imageData.width; x++) {
+                        if (x !== 0)
+                            patternString += ' '
+
+                        const offset = (y * imageData.width * 4) + (x * 4) + channelOffset;
+                        const value = imageData.data[offset];
+                        patternString += String(value).padStart(3);
+                    }
+
+                    patternString += '\n';
+                }
+            }
+        }
+
+        return patternString;
+    }
 }
