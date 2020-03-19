2f22d9c2db6f0d60299ef0ebbb6ffc056251b0a9
leo <mainardi.leo@gmail.com>
2020-03-19 16:04:43 +0100

    - chore: ditto

--- src/modules/location/index.js
+++ src/modules/location/index.js
@@ -1,15 +1,17 @@
+import Handlebars from 'handlebars';
+
 export class LocationModule {
 
-  generateHtml(asset_src, longitude, latitude, path = 'index.html') {
-    const html = Handlebars.templates.locationBased({
-      asset_src: asset_src,
-      longitude: longitude,
-      latitude: latitude
-    });
+    generateHtml(asset_src, longitude, latitude, path = 'index.html') {
+        const html = Handlebars.templates.locationBased({
+            asset_src,
+            longitude,
+            latitude,
+        });
 
-    return {
-      path: path,
-      content: html
-    };
-  }
+        return {
+            path,
+            content: html,
+        };
+    }
 }
